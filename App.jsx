3717a2a8c218ff7ac4d8102721e76cc11d7fe3a6
NANDHAKUMAR BM <138647050+NANDHAKUMAR16@users.noreply.github.com>
2024-03-02 22:27:57 +0530

    App.jsx

--- App.jsx
+++ App.jsx
@@ -6,27 +6,49 @@ export const App = () => {
   const [count, setCount] = useState(0);
   const [intervalID, setintervalID] = useState(null);
   const [AnswerCount, setAnswerCount] = useState(0);
-  const [check, setCheck] = useState(1);
+  const [check, setCheck] = useState(0);
   useEffect(() => {
-    const interval = setInterval(() => {
-      setSeconds((seconds) => seconds - 1);
-      setintervalID(interval);
-    }, 1000);
-    return () => clearInterval(interval);
-  }, []);
+    if (check == 1) {
+      const interval = setInterval(() => {
+        setSeconds((seconds) => seconds - 1);
+        setintervalID(interval);
+      }, 1000);
+      return () => clearInterval(interval);
+    }
+  }, [check]);
 
   const finish = () => {
     clearInterval(intervalID);
-    setCheck(0);
+    setCheck(2);
   }
   useEffect(() => {
-    if (seconds < 1) finish();
+    if (seconds < 1) questionChange();
   }, [seconds]);
 
+  const startTheQuiz = () => {
+    setCheck(1);
+  }
+
+  const questionChange = () => {
+    if (count + 1 < datas.length) {
+      setCount((count) => count + 1);
+      setSeconds(10);
+    }
+    else {
+      finish();
+    }
+  }
   const questionHandleChange = (e) => {
     setSeconds(10);
-    (e.target.textContent.toLowerCase() === datas[count]["answer"].toLowerCase()) ? setAnswerCount(AnswerCount + 1) : "";
+    (e.target.textContent.toLowerCase() === datas[count]["answer"].toLowerCase()) ? setAnswerCount((AnswerCount) => AnswerCount + 1) : "";
     (count + 1 < datas.length) ? setCount((count) => count + 1) : finish();
+  }
+  const resetTheQuiz = () => {
+    setSeconds(10);
+    setCount(0);
+    setintervalID(null);
+    setAnswerCount(0)
+    setCheck(1);
   }
   return (
     <>
@@ -49,9 +71,20 @@ export const App = () => {
             </>)}
           {
             (check == 0) && (
-              <>
-                {"Score "}{AnswerCount}
-              </>
+              <div className="btn">
+                <button style={{ padding: "3px 30px", backgroundColor: "transparent", border: "none", cursor: "pointer" }} onClick={startTheQuiz}>CLICK TO START THE QUIZ</button>
+              </div>
+            )
+          }
+          {
+            (check == 2) && (
+              <div style={{ textAlign: "center", fontSize: "20px", fontWeight: "600" }}>
+                <header>{"Thank you !"}</header>
+                {"Score "}{AnswerCount + 1}
+                <div>
+                  <button onClick={resetTheQuiz} style={{ padding: "3px 30px", backgroundColor: "transparent", border: "none", cursor: "pointer" }}>RESTART THE GAME</button>
+                </div>
+              </div>
             )
           }
         </div>
